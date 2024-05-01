2c12798a5c5445ae8b3803fce5bd39e4c2b2d06c
tgon2993 <tgon2993@uni.sydney.edu.au>
2024-05-02 00:19:16 +1000

    Week 8 Quiz content
    I did the quiz for week 8 in the readme.

--- README.md
+++ README.md
@@ -1,43 +1,25 @@
 # tgon2993_9103_tut6
-My first repository for IDEA9103
+# Imaging Technique Inspiration
+![An image of the Pluto](Assets/Pluto.jpg)
+![An gif image of the Pluto](Assets/Ø.gif)
 
-This is my first local change to the repo!
+## The imaging technique of choice
+These two pictures use different colors of lines, through a certain regular arrangement, to show a special visual effect. This technique is incorporated into subsequent projects and used to create abstract images or dynamic effects that enhance the visual appeal and artistic sense of the work.
 
-# Header 1
-## Header 2
-### Header 3
-#### Header 4
-##### Header 5
-###### Header 6
+## Selection Reason
+This technique will be useful for subsequent projects. Because of their line drawing methods and dynamic effects, they can effectively help me draw artworks. In addition, they also help to add a unique artistic style and expression to the work, improving the art and creativity of the work.
 
-**Bold Text** or __Bold Text__
-*Italic Text* or _Italic Text_
+# Coding Technique Exploration
 
-- Item 1
-- Item 2
-  - Subitem 2.1
-  - Subitem 2.2
+## How to help
+This code example uses the random number generation, Angle calculation and color pattern functions in the p5.js library to draw lines. It can be used as a basis to refer to the line drawing techniques in it and combine with other techniques to achieve the desired effect of drawing line art in the next project.
 
-1. First Item
-2. Second Item
-3. Third Item
+For example, we can adjust the parameters in the createPattern function to control the shape and color of the lines, so that the resulting pattern is closer to the desired effect.
 
-[Link Text](https://www.google.com)
+![An image of Example](Assets/part_2.png)
+[Link Text](https://editor.p5js.org/ICantRemember/sketches/QqjpuLphA)
 
-![An image of a cat](http://placekitten.com/200/300)
+## Refence
+> P5.js Showcase. (n.d.). Retrieved May 1, 2024, from https://showcase.p5js.org/#/2022-All/pattern-creator/
+> Rafal Bromirski: Behance. (n.d.). Retrieved May 1, 2024, from https://www.behance.net/rafalbromirski
 
-![An image of the Mona Lisa](Assets/Mona_Lisa.jpg)
-
-```
-function helloWorld() {
-console.log("Hello, world!");
-}
-```
-
-> This is a blockquote.
-
->this is 
->over multiple lines
-
-To escape formatting, use a **backlash**
-"\\"
--- p5.js/sketch.js
+++ p5.js/sketch.js
@@ -1,24 +1,34 @@
 let img;
 let segments = [];//where we will store each segment
 let numSegments = 50; //how many segments to create
+let drawSegments = false;
+
+let imgDrwPrps = {aspect: 0, width: 0, height: 0, xOffset:0, yOffset:0};
+
+let canvasAspectRatio = 0;
 
 let pixeColour;
-let drawSegments = true;
 
 function preload() {
   img = loadImage("Assets/Mona_Lisa.jpg");
 }
 
 function setup() {
-  createCanvas(img.width, img.height);
+  createCanvas(windowWidth, windowHeight);
 
+  canvasAspectRatio = width/height;
+  imgDrwPrps.aspect = img.width/img.height;
+  calculateImageDrawProps();
+
+  //calculate widht and height of the segments
   let segWidth = img.width/numSegments;
   let segHeight = img.height/numSegments;
 
   for (let yPos = 0; yPos < img.height; yPos += segHeight) {
     for (let xPos = 0; xPos < img.width; xPos += segWidth) {
       let fillColor = img.get(xPos + segWidth/2, yPos +segHeight/2);
-      let segment = new ImageSegment(xPos, yPos, segWidth, segHeight, fillColor);
+      let segment = new ImageSegment(xPos/segWidth, yPos/segHeight, fillColor);
+      segment.calculateSegDrawProps();
       segments.push(segment); //add the segment to the end of the segments array
     }
   }
@@ -26,18 +36,41 @@ function setup() {
   pixeColour = color(0);
 }
 
+function calculateImageDrawProps() {
+  if (imgDrwPrps.aspect > canvasAspectRatio) { //case where image is wider than canvas
+    //draw the image to the width of the canvas
+    imgDrwPrps.width = width;
+    imgDrwPrps.height = width/imgDrwPrps.aspect;
+    imgDrwPrps.xOffset = 0;
+    imgDrwPrps.yOffset = (height - imgDrwPrps.height) / 2;
+
+  } else if (imgDrwPrps.aspect < canvasAspectRatio) {
+    imgDrwPrps.width = height * imgDrwPrps.aspect;
+    imgDrwPrps.height = height;
+    imgDrwPrps.xOffset = (width - imgDrwPrps.width) / 2;
+    imgDrwPrps.yOffset = 0;
+
+  } else if (imgDrwPrps.aspect == canvasAspectRatio) {
+    imgDrwPrps.width = width;
+    imgDrwPrps.height = height;
+    imgDrwPrps.xOffset = 0;
+    imgDrwPrps.yOffset = 0;
+  }
+}
+
 function draw() {
+  background(0);
   if (drawSegments) {
     for (const segment of segments) {
       segment.draw();
     }
   } else {
-    image(img, 0, 0);
+    image(img, imgDrwPrps.xOffset, imgDrwPrps.yOffset, imgDrwPrps.width, imgDrwPrps.height);
   }
 
-  stroke(255);
-  fill(pixeColour);
-  circle(mouseX, mouseY, 40);
+  // stroke(255);
+  // fill(pixeColour);
+  // circle(mouseX, mouseY, 40);
 }
 
 function mouseMoved() {
@@ -50,14 +83,36 @@ function keyPressed() {
   }
 }
 
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  canvasAspectRatio = width/height;
+  calculateImageDrawProps();
+
+  segments.forEach(segment => {
+    segment.calculateSegDrawProps();
+  });
+}
+
 class ImageSegment {
 
-  constructor(xPos, yPos, width, height, fillColor) {
+  constructor(rowPosIn, colPosIn, fillColor) {
+    this.rowPos = rowPosIn;
+    this.colPos = colPosIn;
+
     this.xPos = xPos;
     this.yPos = yPos;
     this.width = width;
     this.height = height;
     this.fillColor = fillColor;
+  }
+
+
+  calculateSegDrawProps() {
+    this.width = imgDrwPrps.width/numSegments;
+    this.height = imgDrwPrps.height/numSegments;
+
+    this.xPos = this.rowPos * this.width + imgDrwPrps.xOffset;
+    this.yPos = this.colPos * this.height + imgDrwPrps.yOffset;
   }
 
   draw() {
